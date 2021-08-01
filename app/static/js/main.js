713ea1cec23feb5d3f82dc22302203ecddb52672
Alex Alves <jhosealex@gmail.com>
2021-08-01 19:53:03 -0300

    Outro teste de _1

---
+++ app/static/js/main.js
@@ -0,0 +1,156 @@
+const CHANNEL = 196384;
+const FIELD_NUMBER_1 = 1;
+const FIELD_NUMBER_2 = 2;
+const FIELD_NUMBER_3 = 3;
+const FIELD_NUMBER_4 = 4;
+
+// Exutando funções quando a tela carregar
+window.onload = function() {
+    // Aguardar enquanto libs são carregadas
+    google.charts.load('current', {'packages':['corechart', 'scatter']});
+    setTimeout(() => {
+        drawChart(FIELD_NUMBER_1);
+        drawChart(FIELD_NUMBER_1, true);
+        drawChart(FIELD_NUMBER_2);
+        drawChart(FIELD_NUMBER_2, true);
+        drawChart(FIELD_NUMBER_3);
+        drawChart(FIELD_NUMBER_3, true);
+        drawChart(FIELD_NUMBER_4);
+        drawChart(FIELD_NUMBER_4, true);
+    }, 500);
+
+    // Atualizar gráficos de 5 em 5 segundos
+    setInterval(() => {
+        drawChart(FIELD_NUMBER_1);
+        drawChart(FIELD_NUMBER_2);
+        drawChart(FIELD_NUMBER_3);
+        drawChart(FIELD_NUMBER_4);
+    }, 5*60000);
+}
+
+
+
+// Buscando dados do ThingSpeak de acordo com o cannel e field
+function pullData(data) {
+    return new Promise(async (resolve, reject) => {
+        
+        axios.get(`https://api.thingspeak.com/channels/${data.channel}/field/${data.field_number}/?results=720&timescale=20`).then(res => {
+            return resolve(res.data);
+        }).catch(err => reject(err));
+        
+    });
+}
+
+// Formatando resposta da api do ThingSpeak
+function formatResponseFromApi(data, field, deviation) {
+    const dataFormat = data.feeds.map((elm, i) => {
+        console.log(Number(calcStandardDeviation(data.feeds.map(elm => Number(elm[field])).splice(0, i+1))));
+        if(deviation) {
+            return [
+                new Date(elm.created_at),
+                Number(calcStandardDeviation(data.feeds.map(elm => Number(elm[field])).splice(0, i+1))),
+            ];
+        } else {
+            return [
+                new Date(elm.created_at),
+                Number(elm[field]),
+                Number(calcAverage(data.feeds.map(elm => Number(elm[field])).splice(0, i+1))),
+            ];
+        }
+    });
+    return dataFormat;
+}
+
+// Obter lista de valores capturados pela api do thingspeak
+function getListOfValues(data, field) {
+    const dataFormat = data.feeds.map(elm => Number(elm[field]));
+    return dataFormat;
+}
+
+// Desenhar gráfico em linhas
+async function drawChart(field_number, deviation) {
+
+    const response = await pullData({ channel: CHANNEL, field_number: field_number });
+    
+    const rows = formatResponseFromApi(response, `field${field_number}`, deviation);
+    var data = new google.visualization.DataTable();
+    if(deviation) {
+        data.addColumn('date');
+        data.addColumn('number', 'Variação')
+    } else {
+        let tempNameField = field_number < 3 ? 'Temperatura' : field_number == 3 ? 'Pressão' : 'Humidade' 
+        data.addColumn('date');
+        data.addColumn('number', tempNameField)
+        data.addColumn('number', 'Média')
+    }
+
+    data.addRows(rows);
+
+    const info_chart = getInfoFields(field_number);
+    
+    var options = {
+        title: info_chart.title,
+        curveType: 'function',
+        legend: { position: 'top'},
+        hAxis: {
+            title: 'Time',
+            format: 'HH:mm',
+        },
+        vAxis: {
+            title: info_chart.vAxisText
+        },
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById(deviation ? `field_${field_number}_deviation` : `field_${field_number}`));
+    
+    chart.draw(data, google.charts.Scatter.convertOptions(options));
+
+    let listOfValues = getListOfValues(response, `field${field_number}`);
+    // console.log(listOfValues)
+    let temperature = listOfValues[listOfValues.length - 1];
+    let standardDeviation = calcStandardDeviation(listOfValues);
+    let average = calcAverage(listOfValues);
+    document.getElementById(`temperature_field_${field_number}`).innerHTML = Number(temperature.toFixed(2));
+    document.getElementById(`deviation_field_${field_number}`).innerHTML = Number(standardDeviation.toFixed(2));
+    document.getElementById(`average_field_${field_number}`).innerHTML = Number(average.toFixed(2));
+    
+    console.log(document.getElementById(`deviation_field_${field_number}`));
+}    
+
+function getInfoFields(field_number) {
+    switch(field_number) {
+        case 1: 
+            return {
+                title: 'Outdoor Temperature (DS18B20)',
+                vAxisText: null
+            }
+        case 2: 
+            return {
+                title: 'Temperature (Si7021)',
+                vAxisText: null//'Temperature [C]'
+            }
+        case 3: 
+            return {
+                title: 'Air Pressure (BMP280)',
+                vAxisText: null //'Temperature [mmHg]'
+            }
+        case 4: 
+            return {
+                title: 'Humidity (Si7021)',
+                vAxisText: null // 'Humidity [%]'
+            }
+    }
+}
+
+// Calcular desvio padrão
+function calcStandardDeviation(listOfValues) {
+    let average = listOfValues.reduce((amount, value) => amount+value/listOfValues.length, 0);
+    let variance = listOfValues.reduce((amount, value) => amount + Math.pow(average - value, 2)/listOfValues.length, 0);
+    let standardDeviation = Math.sqrt(variance);
+    return standardDeviation;
+}
+
+// Calcular média
+function calcAverage(listOfValues) {
+    return (listOfValues.reduce((amount, value) => amount + value, 0))/listOfValues.length;
+}
---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import requests
+import json
+# from prophet import Prophet
+
+# Init
+data = requests.get('https://api.thingspeak.com/channels/196384/field/1/?results=720&timescale=20').json()
+dataMap = list(map(lambda x: { "Month":  x["created_at"][:10], "Values": x["field1"] }, data["feeds"]))
+feeds = json.dumps(dataMap)
+
+print(feeds)
+
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
