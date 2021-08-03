95e0a79d56855a47a9ec665644aa79938bb21c29
Alex Alves <jhosealex@gmail.com>
2021-08-03 14:36:40 -0300

    Calculando field 2 também

--- app/views.py
+++ app/views.py
@@ -13,10 +13,10 @@ import threading
 # Init
 test = {}
 
-def init():
+def init(field_number):
     print('Init')
-    data = requests.get('https://api.thingspeak.com/channels/196384/field/1/?results=720&timescale=20').json()
-    dataMap = list(map(lambda x: { "Month":  x["created_at"][:19], "Values": x["field1"] }, data["feeds"]))
+    data = requests.get('https://api.thingspeak.com/channels/196384/field/'+ str(field_number) +'/?results=500&timescale=20').json()
+    dataMap = list(map(lambda x: { "Month":  x["created_at"][:19], "Values": x["field"+str(field_number)] }, data["feeds"]))
     feeds = json.dumps(dataMap)
 
     df = pd.read_json(feeds)
@@ -31,11 +31,11 @@ def init():
     model.fit(df)
 
     future = list()
-    now = datetime.now() + timedelta(minutes=5)
-    for i in range(5):
+    now = datetime.now() + timedelta(hours=3) + timedelta(minutes=5)
+    for i in range(6):
         current_time = now.strftime("%Y-%m-%d %H:%M")
         future.append([current_time])
-        now = now + timedelta(minutes=5)
+        now = now + timedelta(minutes=20)
         
     future = DataFrame(future)
     future.columns = ['ds']
@@ -48,14 +48,16 @@ def init():
 
     result = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_json(orient="records")
     parsed = json.loads(result)
-    test["data"] = json.dumps(parsed)
-    print(parsed)
 
     # Create your views here.
-    with open('./app/static/data.json', 'w', encoding='utf-8') as f:
+    with open('./app/static/field'+str(field_number)+'.json', 'w', encoding='utf-8') as f:
         json.dump(parsed, f, ensure_ascii=False, indent=4)
 
+    print(datetime.now())
 
+def start():
+    init(1)
+    init(2)
 
 def set_interval(func, sec):
     def func_wrapper():
@@ -65,7 +67,7 @@ def set_interval(func, sec):
     t.start()
     return t
 
-init()
-set_interval(init, 5*60)
+start()
+set_interval(start, 5*60)
 def home(request):
-    return render(request, 'index.html', test)
+    return render(request, 'index.html')
